8f85f87284f41e63f5b5adfe6ac747d61fddbf12
amandagong <amandagong@hunterschools.org>
2021-01-25 18:20:56 +0000

    a??

--- main.py
+++ main.py
@@ -1,47 +1,56 @@
 import random
 import os
 
+
 def win():
   os.system("clear")
-  print("you won")
+  print("you won.")
 def lose():
-  print("you lose")
+  os.system("clear")
+  print("you lose, no more body parts for you :(")
+def setup():
+  os.system("clear")
+  print("input the words 'final guess' at any time to make a final guess")
+  print("current wrong guesses:")
+  print(wrongguesses)
+  print("you have %d body parts left" % (8-len(wrongguesses)))
+  print(displayedword)
 
-words = ["protist", "jeopardy", "attachment", "poker", "bamboo", "crustacean", "cerulean", "mulch", "cornea"]
+words = ["bamboo", "cerulean", "mulch", "jeopardy", "attachment", "poker", "cornea", "trousers", "copper", ""]
 word = random.choice(words)
 wordlist = list(word)
 displayedword = ("-"*len(word))
 displayedlist = list(displayedword)
 wrongguesses = []
 
-print("input the words 'final guess' at any time to make a final guess")
-
-
-
 while len(wrongguesses) < 8 or displayedword != word:
-  print("you have %d body parts left" % (8-len(wrongguesses)))
-  print(displayedword)
-  guess = str(input("Guess a letter: "))
-  if guess == "final guess":
-    final = input("Make your final guess: ")
-    if final == str(word):
-      win()
-    else:
-      "wrong."
+  if displayedword == word:
+    win()
+    break
+  elif len(wrongguesses) == 8:
+    lose()
+    break
   else:
-    if displayedword == word:
-      win()
-    elif len(guess) == 1 and guess not in wrongguesses:
-      for i in wordlist:
+    setup()
+    guess = input("Guess a letter: ")
+    if guess == "final guess":
+      final = input("Make your final guess: ")
+      if final == word:
+        win()
+        break
+      else:
+        setup()
+    else:
+      if len(guess) == 1 and guess not in wrongguesses:
         if guess in wordlist:
-         foundindex = wordlist.index(guess)
-         displayedlist[foundindex] = guess
-         displayedword = "".join(displayedlist)
+          for i in range(len(wordlist)):
+            if wordlist[i] == guess:
+              displayedlist[i] = guess
+              displayedword = "".join(displayedlist)
+          setup()
         else:
           if guess not in wrongguesses:
            wrongguesses.append(guess)
-           print("wrong guess. current wrong guesses:")
-           print(wrongguesses)
-    else:
-      print("you already chose that letter or that is not a letter")
-lose()
+          setup()
+      else:
+        setup()
